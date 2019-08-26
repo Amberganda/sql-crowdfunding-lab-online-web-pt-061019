a5d5b019c12eb8c3357ebdcabeb3f902157d4bf6
Amber Taranto <ambzzzz@gmail.com>
2019-08-26 04:33:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -51,7 +51,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
     SELECT projects.category, pledges.amount
     FROM projects
     INNER JOIN pledges on projects.id = pledges.project_id
-    GROUP BY projects.category
+    GROUP BY projects.title
     HAVING projects.category = 'music'
 
 
